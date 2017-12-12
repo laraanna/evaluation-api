b962545ac744a693ed510bdbe433202355b984a8
laraanna <lara@laraannawagner.com>
2017-12-12 19:55:17 +0100

    Set up Project Api

---
+++ routes/batches.js
@@ -0,0 +1,51 @@
+// routes/batches.js
+const router = require('express').Router()
+const { Batch } = require('../models')
+const passport = require('../config/auth')
+
+router.get('/batches', (req, res, next) => {
+  Batch.find()
+    // Newest batches first
+    .sort({ createdAt: -1 })
+    // Send the data in JSON format
+    .then((batches) => res.json(batches))
+    // Throw a 500 error if something goes wrong
+    .catch((error) => next(error))
+  })
+  .get('/batches/:id', (req, res, next) => {
+    const id = req.params.id
+    Batch.findById(id)
+      .then((batch) => {
+        if (!batch) { return next() }
+        res.json(batch)
+      })
+      .catch((error) => next(error))
+  })
+  .post('/batches',
+    passport.authorize('jwt', { session: false }),
+    (req, res, next) => {
+      let newBatch = req.body
+      newBatch.authorId = req.account._id
+
+      Batch.create(newBatch)
+        .then((batch) => res.json(batch))
+        .catch((error) => next(error))
+    })
+  .put('/batches/:id', (req, res, next) => {
+    const id = req.params.id
+    Batch.findById(id)
+      .then((batch) => {
+        if (!batch) { return next() }
+
+        const newData = req.body
+
+        batch.update(newData)
+          .then((updatedBatch) => {
+            res.json(updatedBatch)
+          })
+          .catch((error) => next(error))
+      })
+      .catch((error) => next(error))
+  })
+
+module.exports = router
